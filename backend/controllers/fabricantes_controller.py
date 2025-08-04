eaf6681ca32d44bc650f994255a30cf2ba8ef003
Marisa <marisa.meurer@gmail.com>
2025-08-04 01:17:08 -0300

    Criar fabricantes_controller.py

---
+++ backend/controllers/fabricantes_controller.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, jsonify, request
+from database import db
+
+fabricantes_controller = Blueprint('fabricantes_controller', __name__)
+fabricantes_collection = db['fabricantes']
+
+# Listar fabricantes
+@fabricantes_controller.route('/fabricantes', methods=['GET'])
+def listar_fabricantes():
+    fabricantes = list(fabricantes_collection.find({}, {'_id': 0}))
+    return jsonify(fabricantes)
+
+# Adicionar fabricante
+@fabricantes_controller.route('/fabricantes', methods=['POST'])
+def adicionar_fabricante():
+    dados = request.get_json()
+    if not dados.get('nome') or not dados.get('produtos'):
+        return jsonify({'erro': 'Campos obrigatórios: nome, produtos'}), 400
+
+    fabricantes_collection.insert_one(dados)
+    return jsonify({'mensagem': 'Fabricante cadastrado com sucesso'})
