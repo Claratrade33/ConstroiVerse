b68eb0e772f378dd3657f9c718177a5feff47242
Marisa <marisa.meurer@gmail.com>
2025-08-04 09:23:26 -0300

    Atualizar o perfil_controller.py

--- backend/controllers/perfil_controller.py
+++ backend/controllers/perfil_controller.py
@@ -4,24 +4,34 @@ from database import db
 perfil_controller = Blueprint('perfil_controller', __name__)
 usuarios_collection = db['usuarios']
 
-# Listar todos os usuários com perfis
-@perfil_controller.route('/perfis', methods=['GET'])
-def listar_perfis():
-    usuarios = list(usuarios_collection.find({}, {'_id': 0}))
-    return jsonify(usuarios)
+# Retorna o painel correto de acordo com o perfil
+@perfil_controller.route('/perfil/<usuario>', methods=['GET'])
+def obter_painel(usuario):
+    usuario_data = usuarios_collection.find_one({'usuario': usuario})
 
-# Cadastrar novo usuário com perfil
-@perfil_controller.route('/perfis', methods=['POST'])
-def cadastrar_perfil():
-    dados = request.get_json()
-    if not dados.get('nome') or not dados.get('perfil'):
-        return jsonify({'erro': 'Campos obrigatórios: nome, perfil'}), 400
+    if not usuario_data:
+        return jsonify({'erro': 'Usuário não encontrado'}), 404
 
-    usuarios_collection.insert_one(dados)
-    return jsonify({'mensagem': 'Perfil cadastrado com sucesso'})
+    perfil = usuario_data.get('perfil')
 
-# Buscar usuários por tipo de perfil
-@perfil_controller.route('/perfis/<perfil>', methods=['GET'])
-def buscar_por_tipo(perfil):
-    resultados = list(usuarios_collection.find({'perfil': perfil}, {'_id': 0}))
-    return jsonify(resultados)
+    rotas = {
+        'arquiteto': '/painel_arquiteto',
+        'engenheiro': '/painel_engenheiro',
+        'loja': '/painel_loja',
+        'fabricante': '/painel_fabricante',
+        'representante': '/painel_representante',
+        'corretor': '/painel_corretor',
+        'mestre': '/painel_mestre',
+        'pedreiro': '/painel_pedreiro',
+        'eletricista': '/painel_eletricista',
+        'encanador': '/painel_encanador',
+        'cliente': '/painel_cliente',
+        'construtora': '/painel_construtora'
+    }
+
+    rota_painel = rotas.get(perfil)
+
+    if not rota_painel:
+        return jsonify({'erro': 'Perfil não reconhecido'}), 400
+
+    return jsonify({'painel': rota_painel})
