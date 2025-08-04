3f122b7dda9e4a8f351a895f86022957763b3d38
Marisa <marisa.meurer@gmail.com>
2025-08-04 09:07:35 -0300

    Atualizar o perfil_controller.py

--- backend/controllers/perfil_controller.py
+++ backend/controllers/perfil_controller.py
@@ -1,15 +1,27 @@
 from flask import Blueprint, jsonify, request
-from config import db
+from database import db
 
-perfil_bp = Blueprint('perfil_bp', __name__)
+perfil_controller = Blueprint('perfil_controller', __name__)
 usuarios_collection = db['usuarios']
 
-@perfil_bp.route('/api/perfis', methods=['GET'])
+# Listar todos os usuários com perfis
+@perfil_controller.route('/perfis', methods=['GET'])
 def listar_perfis():
     usuarios = list(usuarios_collection.find({}, {'_id': 0}))
     return jsonify(usuarios)
 
-@perfil_bp.route('/api/perfis/<string:tipo>', methods=['GET'])
-def listar_por_tipo(tipo):
-    usuarios = list(usuarios_collection.find({'tipo': tipo}, {'_id': 0}))
-    return jsonify(usuarios)
+# Cadastrar novo usuário com perfil
+@perfil_controller.route('/perfis', methods=['POST'])
+def cadastrar_perfil():
+    dados = request.get_json()
+    if not dados.get('nome') or not dados.get('perfil'):
+        return jsonify({'erro': 'Campos obrigatórios: nome, perfil'}), 400
+
+    usuarios_collection.insert_one(dados)
+    return jsonify({'mensagem': 'Perfil cadastrado com sucesso'})
+
+# Buscar usuários por tipo de perfil
+@perfil_controller.route('/perfis/<perfil>', methods=['GET'])
+def buscar_por_tipo(perfil):
+    resultados = list(usuarios_collection.find({'perfil': perfil}, {'_id': 0}))
+    return jsonify(resultados)
