f723cb0867d78dcfb45220988ce26bd1cf46b2fc
Marisa <marisa.meurer@gmail.com>
2025-08-05 02:15:38 -0300

    Atualizar o config.py

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,6 @@
 import os
-from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
-
-client = MongoClient(MONGO_URI)
-db = client['constroiverse']
+SECRET_KEY = os.getenv("SECRET_KEY", "chave_padrao_secreta")
