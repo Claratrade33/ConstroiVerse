710ec0036998a293467c139fc941c9c9c174d4e1
Marisa <marisa.meurer@gmail.com>
2025-08-05 15:11:36 -0300

    Atualizar o database.py

--- backend/database.py
+++ backend/database.py
@@ -5,5 +5,35 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
-db = client["constroiverse"]
+
+if MONGO_URI:
+    client = MongoClient(MONGO_URI)
+    db = client["constroiverse"]
+else:
+    class _FakeCollection(dict):
+        def find_one(self, query):
+            return next(
+                (doc for doc in self.values() if all(doc.get(k) == v for k, v in query.items())),
+                None,
+            )
+
+        def insert_one(self, doc):
+            doc = dict(doc)
+            doc["_id"] = len(self) + 1
+            self[doc["_id"]] = doc
+
+            class _Result:
+                inserted_id = doc["_id"]
+
+            return _Result()
+
+        def delete_many(self, query):
+            keys = [k for k, v in self.items() if all(v.get(k2) == v2 for k2, v2 in query.items())]
+            for k in keys:
+                del self[k]
+
+    class _FakeDB:
+        def __init__(self):
+            self.users = _FakeCollection()
+
+    db = _FakeDB()
